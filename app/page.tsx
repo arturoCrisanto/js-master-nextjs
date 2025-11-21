a5f70d936ffdb55b468c5173dc1e51d68d5b3d0c
ruzell <arturocrisanto1@gmail.com>
2025-11-22 01:17:11 +0800

    Add ExploreBtn component and update page layout with featured events

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import ExploreBtn from "@/components/ExploreBtn"
 
 function page() {
   return (
   <section>
     <h1 className="text-center">The Hub for Every Event <br/>Event you cant miss </h1>
     <p className="text-center mt-5">Hacathons, Meetups, and Conferences, All in one Place </p>
+    <ExploreBtn />
+
+    <div className="mt-20 space-y-7">
+
+      <h3>Featured Events</h3>
+      <ul className="events list-style-none">
+        {[1,2,3,4,5,6,7,8].map((event)=>(
+          <li key={event}> Event {event}
+          </li>
+        ))}
+        
+      </ul>
+
+    </div>
   </section>
   )
 }
